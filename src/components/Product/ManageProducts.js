247cc6849fa56a6bc311725263316bf38ce01754
unknown <jorammanoah1@gmail.com>
2021-04-17 18:34:02 -0700

    Modified Manage Product

--- src/components/Product/ManageProducts.js
+++ src/components/Product/ManageProducts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Button, Modal, Input, Form, Upload, message } from "antd";
+import {  Button, Modal, Input, Form, Upload,Image, message,Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import {
   deleteProduct,
@@ -7,6 +7,38 @@ import {
   updateProduct,
 } from "../../_actions/productActions";
 import { UploadOutlined } from "@ant-design/icons";
+
+const columns = [
+  {
+    title: "id ",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "product_name",
+    dataIndex: "product_name",
+    key: "product_name",
+  },
+  {
+    title: "price",
+    dataIndex: "price",
+    key: "price",
+  },
+  {
+    title: "category",
+    dataIndex: "category",
+    key: "category",
+  },
+  {
+    title: "image",
+    dataIndex: "image",
+    key: "image",
+    render: (img) => (
+      <Image src={img} alt="image file" style={{ width: "50px" }} />
+    ),
+  },
+];
+
 
 export default function ManageProducts(props) {
   const ProductList = useSelector((state) => state.productList);
@@ -115,43 +147,7 @@ export default function ManageProducts(props) {
       ) : error ? (
         <div>{error}</div>
       ) : (
-        <Row justify="space-around" align="middle">
-          <Col>
-            <table>
-              <thead>
-                <th>id</th>
-                <th>name</th>
-                <th>price</th>
-                <th>category</th>
-                <th>image</th>
-                <th>action</th>
-              </thead>
-              <tbody>
-                {posts.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.product_name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.category}</td>
-                    <td>
-                      <img
-                        src={item.image}
-                        style={{ width: "100px" }}
-                        alt="dosjfljdsfdj"
-                      />
-                    </td>
-                    <td>
-                      <Button onClick={() => showModal(item)}>Edit</Button>
-                      <Button onClick={() => deleteHandler(item.id)}>
-                        Delete
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Col>
-        </Row>
+        <Table dataSource={posts} columns={columns}/>
       )}
       <Modal
         title="Edit products"

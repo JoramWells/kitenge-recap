a1f65825e35fff26895d521711ea6d19cf3c65b7
unknown <jorammanoah1@gmail.com>
2021-04-17 17:08:38 -0700

    mobile navbar

--- src/components/HomeRoutes.js
+++ src/components/HomeRoutes.js
@@ -1,7 +1,10 @@
 import React, { lazy } from "react";
-const DesktopMobile = lazy(() => import("./DesktopMobile"));
-const RecentItemsBar = lazy(() => import("./RecentItemsBar"));
-const CarouselHeader = lazy(() => import("./Desktop/CarouselHeader"));
+// const DesktopMobile = lazy(() => import("./DesktopMobile"));
+// const RecentItemsBar = lazy(() => import("./RecentItemsBar"));
+// const CarouselHeader = lazy(() => import("./Desktop/CarouselHeader"));
+import CarouselHeader from './Desktop/CarouselHeader'
+import RecentItemsBar from './RecentItemsBar'
+import DesktopMobile from './DesktopMobile'
 
 export default function HomeRoutes() {
   return (
--- src/components/MobileView/NavMobile.js
+++ src/components/MobileView/NavMobile.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 import {
-  MailOutlined,
-  ShopOutlined,
   UserOutlined,
   HomeOutlined,
   SearchOutlined,
 } from "@ant-design/icons";
 import { Menu, Badge } from "antd";
+import { useSelector } from "react-redux";
+import Cookie from "js-cookie";
 
 const { SubMenu } = Menu;
+const cartItems = Cookie.getJSON("cartItems");
+
 export default function NavMobile() {
-  return (
-    <div>
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const [visible, setVisible] = useState(false);
+  const PaymentList = useSelector((state) => state.paymentList);
+  const { payments } = PaymentList;
+  if(!userInfo){
+    return(
+      <nav>
       <Menu
         mode="horizontal"
-        style={{ justifyContent: "space-between", border: "0" }}
+        style={{  border: "0" }}
       >
         <Menu.Item title="Home">
+          <a href="/">
           <HomeOutlined style={{ fontSize: "1.5rem" }} />
+
+          </a>
         </Menu.Item>
         <Menu.Item
           icon={
@@ -31,16 +42,103 @@ export default function NavMobile() {
             Cart
           </Badge>
         </Menu.Item>
-        <Menu.Item>
-          <Badge count={2}>Payments</Badge>
-        </Menu.Item>
+        
         <Menu.Item
           icon={<SearchOutlined style={{ fontSize: "1.5rem" }} />}
-        ></Menu.Item>
-        <SubMenu title={<UserOutlined style={{ fontSize: "1.5rem" }} />}>
-          <Menu.Item>Logout</Menu.Item>
-        </SubMenu>
+        >Search</Menu.Item>
+                <Menu.Item
+          icon={<SearchOutlined style={{ fontSize: "1.5rem" }} />}
+        >Signup</Menu.Item>
+
       </Menu>
-    </div>
-  );
+    </nav>
+    )
+  }else{
+    if(!cartItems){
+      return (
+        <nav>
+          <Menu
+            mode="horizontal"
+            style={{ justifyContent: "space-between", border: "0" }}
+          >
+            <Menu.Item title="Home">
+              <HomeOutlined style={{ fontSize: "1.5rem" }} />
+            </Menu.Item>
+            <Menu.Item
+              icon={
+                <Badge dot count={0} style={{ backgroundColor: "green" }}>
+                  Inbox
+                </Badge>
+              }
+            ></Menu.Item>
+            <Menu.Item>
+              <Badge dot count={2}>
+                Cart
+              </Badge>
+            </Menu.Item>
+            <Menu.Item>
+              <Badge count={2}>Payments</Badge>
+            </Menu.Item>
+            <Menu.Item
+              icon={<SearchOutlined style={{ fontSize: "1.5rem" }} />}
+            >Search</Menu.Item>
+            <SubMenu title={<UserOutlined style={{ fontSize: "1.5rem" }} />}>
+            <Menu.Item style={{ paddingLeft: "85px", margin: "auto" }}>
+                  <img
+                    src={userInfo.avatar}
+                    alt="profile_pic"
+                    style={{ width: "25px", borderRadius: "50px" }}
+                  />
+                </Menu.Item>
+              <Menu.Item>{userInfo.email}</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </nav>
+      );
+    }else{
+      return (
+        <nav>
+          <Menu
+            mode="horizontal"
+            style={{ justifyContent: "space-between", border: "0" }}
+          >
+            <Menu.Item title="Home">
+              <HomeOutlined style={{ fontSize: "1.5rem" }} />
+            </Menu.Item>
+            <Menu.Item
+              icon={
+                <Badge dot count={1} style={{ backgroundColor: "green" }}>
+                  Inbox
+                </Badge>
+              }
+            ></Menu.Item>
+            <Menu.Item>
+              <Badge dot count={2}>
+                Cart
+              </Badge>
+            </Menu.Item>
+            <Menu.Item>
+              <Badge count={2}>Payments</Badge>
+            </Menu.Item>
+            <Menu.Item
+              icon={<SearchOutlined style={{ fontSize: "1.5rem" }} />}
+            >Search</Menu.Item>
+            <SubMenu title={<UserOutlined style={{ fontSize: "1.5rem" }} />}>
+            <Menu.Item style={{ paddingLeft: "85px", margin: "auto" }}>
+                  <img
+                    src={userInfo.avatar}
+                    alt="profile_pic"
+                    style={{ width: "25px", borderRadius: "50px" }}
+                  />
+                </Menu.Item>
+              <Menu.Item>{userInfo.email}</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </nav>
+      );
+      
+    }
+
+  }
+
 }
